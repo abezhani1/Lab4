72977cecc84e8699b9a538fe6fcf0e915dedab0d
abezhani1 <abezhani@my.wctc.edu>
2018-03-12 20:17:39 -0500

    Initial commit

---
+++ src/edu/abezhani/Main.java
@@ -0,0 +1,10 @@
+package edu.abezhani;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Lab4 will be found in the other pages since they all require a main class.
+
+        lab4 = new Employee();
+    }
+}
---
+++ src/edu/abezhani/Startup.java
@@ -0,0 +1,26 @@
+package edu.abezhani;
+
+/**
+ * This class is the startup class for the program. But together with the
+ * other classes provided it is not a particularly good simulation of the
+ * real world. Employees don't just hire themselves and them tell themselves
+ * to go through orientation. There's usually a company and a HR person involved.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.02
+ */
+public class Startup
+{
+    public static void main(String[] args)
+    {
+        //not sure what to do here, solution suggested to make a company class?
+        Employee employee = new Employee("Peter","Piper","333-33-3333");
+
+        employee.doFirstTimeOrientation("A101");
+        employee.getReportService().outputReport();
+
+
+    }
+
+
+}
